fa2b44e59c12f064f4337d07e6ba479b2368b352
Cullen Jett <cullenjett@gmail.com>
2019-07-18 20:45:43 -0600

    move head manipulation to normal component instead of json-ish data in a separate file

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,24 @@
 import React from 'react';
 import { Switch, Route, NavLink } from 'react-router-dom';
 import Loadable from 'react-loadable';
-import Helmet from 'react-helmet';
 
-import * as metadata from '../metadata';
+import Head from './Head';
 
 const LoadableHome = Loadable({
   loader: () => import(/* webpackChunkName: 'home' */ './Home'),
-  loading() {
-    return <div>Loading...</div>;
-  }
+  loading: () => <div>Loading...</div>
 });
 
 const LoadableAbout = Loadable({
   loader: () => import(/* webpackChunkName: 'about' */ './about/About'),
-  loading() {
-    return <div>Loading...</div>;
-  }
+  loading: () => <div>Loading...</div>
 });
 
 const App = () => (
   <div className="app">
-    <Helmet
-      title={metadata.title}
-      meta={metadata.meta}
-      link={metadata.link}
-      script={metadata.script}
-      noscript={metadata.noscript}
-    />
+    <Head />
 
-    <nav>
+    <nav aria-label="main navigation">
       <NavLink exact to="/" activeClassName="active">
         Home
       </NavLink>{' '}
@@ -38,12 +27,12 @@ const App = () => (
       </NavLink>
     </nav>
 
-    <div className="main">
+    <main className="main">
       <Switch>
         <Route exact path="/" component={LoadableHome} />
         <Route path="/about" component={LoadableAbout} />
       </Switch>
-    </div>
+    </main>
 
     <footer />
   </div>
