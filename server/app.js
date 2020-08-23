54f2870d76c12439bb5b6e6914ae9a9a2e72f69c
Cullen Jett <cullenjett@gmail.com>
2020-08-22 18:41:30 -0600

    remove response time middleware

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ import express from 'express';
 import compression from 'compression';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import responseTime from 'response-time';
 import bodyParser from 'body-parser';
 
 import { renderServerSideApp } from './renderServerSideApp';
@@ -42,12 +41,5 @@ app.use(morgan('tiny'));
 
 // Demo API endpoints
 app.use(todoRoutes());
-
-app.use(
-  responseTime((_req, res, time) => {
-    res.setHeader('X-Response-Time', time.toFixed(2) + 'ms');
-    res.setHeader('Server-Timing', `renderServerSideApp;dur=${time}`);
-  })
-);
 
 app.use(renderServerSideApp);
